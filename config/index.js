5679b0fc4bb08743bcba9c7221bd4c79fa48fd7a
xiongqiang <qxiong@optikom.cn>
2021-03-09 10:42:27 +0800

    chore: 配置vue.config.js并打包成功

---
+++ config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  development: require('./vue.config.dev'),
+  production: require('./vue.config.prd')
+}
---
+++ server.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  servers: {
+    online: {
+      domain: '//woowalker.com'
+    },
+    beta: {
+      domain: '//beta1.woowalker.com'
+    }
+  }
+}
